f01b24a643738c2d8af2e30c359ae2086e38d640
CarlaMaHe <152259810+CarlaMaHe@users.noreply.github.com>
2024-02-09 10:15:17 +0000

    update req.txt

--- Hello.py
+++ Hello.py
@@ -1,23 +1,11 @@
 import streamlit as st
-st.write('Hello World')
-
-import streamlit as st
-
-option = st.selectbox(
-    '',
-    ('private Email', 'working Email', 'Home phone', 'Mobile phone'))
-
-st.write('You selected:', option)
-
-
-import streamlit as st
 import pandas as pd
 import numpy as np
-
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-
-st.line_chart(chart_data)
-
 import matplotlib.pyplot as plt
+import os
 
-st.pyplot(chart_data)
+# Datei einlesen 
+file_path = '/content/drive/MyDrive/Mikroanalytik/Data /Projekt_rohDaten.xlsx'
+basename = os.path.basename(file_path)
+file_name = os.path.splitext(basename)[0]
+print(file_name)
